a6f75b6b45970e5c2a2e4851dcc0786ba1748e26
Tlacaelel <tlacaelel.flores@ciencias.unam.mx>
2022-08-13 11:39:25 -0500

    coding styles

--- Introductory/BasicUsage.py
+++ Introductory/BasicUsage.py
@@ -68,6 +68,60 @@ fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
 ax.scatter('a', 'b', c='c', s='d', data=data)
 ax.set_xlabel('entry a')
 ax.set_ylabel('entry b')
+
+# %% CODING STYLES
+"""
+Tenemos basicamente dos formas de usar MAtplotlib:
+        Explicitly create Figures and Axes, and call methods on them (the "object-oriented (OO) style").
+
+        Rely on pyplot to automatically create and manage the Figures and Axes, and use pyplot functions 
+        for plotting.
+"""
+"""Mediante OO-style"""
+x = np.linspace(0, 2, 100)  # Sample data.
+
+# Note that even in the OO-style, we use `.pyplot.figure` to create the Figure.
+fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
+ax.plot(x, x, label='linear')  # Plot some data on the axes.
+ax.plot(x, x**2, label='quadratic')  # Plot more data on the axes...
+ax.plot(x, x**3, label='cubic')  # ... and some more.
+ax.set_xlabel('x label')  # Add an x-label to the axes.
+ax.set_ylabel('y label')  # Add a y-label to the axes.
+ax.set_title("Simple Plot")  # Add a title to the axes.
+ax.legend();  # Add a legend.
+
+"""Mediante pyplot-style"""
+x = np.linspace(0, 2, 100)  # Sample data.
+
+plt.figure(figsize=(5, 2.7), layout='constrained')
+plt.plot(x, x, label='linear')  # Plot some data on the (implicit) axes.
+plt.plot(x, x**2, label='quadratic')  # etc.
+plt.plot(x, x**3, label='cubic')
+plt.xlabel('x label')
+plt.ylabel('y label')
+plt.title("Simple Plot")
+plt.legend();
+
+"""
+OO style es muy util para funciones complicadas y scripts para
+proyectos grandes, mienrtas que pyplot puede ser usada para 
+graficaciones rápidas
+"""
+
+#%% MAKING HELPER FUNCTIONS
+
+def my_plotter(ax, data1, data2, param_dict):
+    """
+    A helper function to make a graph.
+    """
+    out = ax.plot(data1, data2, **param_dict)
+    return out
+
+#usamos esta funcion para hacer las siguientes gráficas
+data1, data2, data3, data4 = np.random.randn(4, 100)  # make 4 random data sets
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(5, 2.7))
+my_plotter(ax1, data1, data2, {'marker': 'x'})
+my_plotter(ax2, data3, data4, {'marker': 'o'});
+
 # Descomentar para mostrar las gráficas en la salida del cmd o powershell
 #plt.show();
-# %%
